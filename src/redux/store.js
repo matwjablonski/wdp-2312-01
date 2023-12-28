e55f2ab3a7d20ca1d453cee0500d9a89fd5f2619
Julia Chanas <juulia.chanas@gmail.com>
2023-12-28 21:56:35 +0100

    Create selectors and reducer for tags and add reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import initialState from './initialState';
 import cartReducer from './cartRedux';
 import categoriesReducer from './categoriesRedux';
 import productsReducer from './productsRedux';
+import tagsReducer from './tagsRedux';
 
 // define reducers
 const reducers = {
   cart: cartReducer,
   categories: categoriesReducer,
   products: productsReducer,
+  tags: tagsReducer,
 };
 
 // add blank reducers for initial state properties without reducers

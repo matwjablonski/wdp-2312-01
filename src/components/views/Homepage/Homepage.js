dd58e738e545074d8b977bebfd3e1dd27e86001c
dave8git <dave8git@gmail.com>
2023-12-28 11:09:43 +0100

    Add changes to PromoContainer.js

--- src/components/views/Homepage/Homepage.js
+++ src/components/views/Homepage/Homepage.js
@@ -6,12 +6,14 @@ import styles from './Homepage.module.scss';
 import FeatureBoxes from '../../features/FeatureBoxes/FeatureBoxes';
 import NewFurniture from '../../features/NewFurniture/NewFurnitureContainer';
 import Promoted from '../../features/Promoted/Promoted';
+import Promo from '../../layout/Promo/PromoContainer';
 
 const Homepage = () => (
   <div className={styles.root}>
     <Promoted />
     <FeatureBoxes />
     <NewFurniture />
+    <Promo />
   </div>
 );
 

b9aeb0bc62a5fda57cd997772fa1eb1c83b14fb4
Julia Chanas <juulia.chanas@gmail.com>
2024-01-01 15:02:43 +0100

    merge changes from prev task to be able to continue this task

--- src/components/common/GalleryBox/SliderBottom/SliderBottom.js
+++ src/components/common/GalleryBox/SliderBottom/SliderBottom.js
@@ -16,6 +16,22 @@ const SliderBottom = ({ activeItems, setActiveIndex }) => {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+    ],
   };
 
   const handleActivePhoto = index => {
